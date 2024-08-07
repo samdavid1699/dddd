53ee6c19ece242ec10c21cf2b51c1aee76673760
Sam David.P <149740836+samdavid1699@users.noreply.github.com>
2024-08-07 11:09:02 +0530

    Add files via upload

---
+++ src/main/java/com/redhat/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.redhat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Controller {
+@GetMapping("/")
+	public String getvalue() {
+		return "Deployed successfully";
+	}
+@PostMapping("/{name}")
+public String getvalue(@PathVariable String name) {
+	return "Your name is " + name;
+}
+}
